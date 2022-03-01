4a908b5184371b2aa77e3050c1418bb0440855e4
NellySP <nelly.petren@gmail.com>
2022-03-01 17:55:13 +0100

    another massive push

--- javascript/index.js
+++ javascript/index.js
@@ -24,3 +24,22 @@ document.addEventListener("scroll", () => {
     about.classList.add("fadeIn");
   }
 });
+
+window.addEventListener("scroll", throttle(parallax, 14));
+
+function throttle(fn, wait) {
+  var time = Date.now();
+  return function () {
+    if (time + wait - Date.now() < 0) {
+      fn();
+      time = Date.now();
+    }
+  };
+}
+
+function parallax() {
+  var scrolled = window.pageYOffset;
+  var parallax = document.querySelector(".parallax");
+  var coords = scrolled * 0.4 + "px";
+  parallax.style.transform = "translateY(" + coords + ")";
+}
