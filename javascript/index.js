09417b566138486c4b5f1f82d7d72f0c54ba21b8
Emma Ramstedt <71063351+Deliciaes@users.noreply.github.com>
2022-03-02 13:18:51 +0100

    parallax library import

--- javascript/index.js
+++ javascript/index.js
@@ -25,21 +25,5 @@ document.addEventListener("scroll", () => {
   }
 });
 
-window.addEventListener("scroll", throttle(parallax, 14));
-
-function throttle(fn, wait) {
-  var time = Date.now();
-  return function () {
-    if (time + wait - Date.now() < 0) {
-      fn();
-      time = Date.now();
-    }
-  };
-}
-
-function parallax() {
-  var scrolled = window.pageYOffset;
-  var parallax = document.querySelector(".parallax");
-  var coords = scrolled * 0.4 + "px";
-  parallax.style.transform = "translateY(" + coords + ")";
-}
+var image = document.getElementsByClassName('thumbnail');
+new simpleParallax(image);
